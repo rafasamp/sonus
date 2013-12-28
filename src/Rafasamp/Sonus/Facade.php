420b18024070dfe57d90cc2867074c16b3499d66
Rafael Sampaio <rafaicm@gmail.com>
2013-12-27 23:57:43 -0500

    Initial commit

---
+++ src/Rafasamp/Sonus/Facade.php
@@ -0,0 +1,16 @@
+<?php
+namespace Rafasamp\Sonus;
+
+use Illuminate\Support\Facades\Facade as IlluminateFacade;
+
+class Facade extends IlluminateFacade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'sonus'; }
+
+
+}
