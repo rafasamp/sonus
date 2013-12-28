8ae707417ca6d484ffd6016ccdb139a37e9fb015
Rafael Sampaio <rafaicm@gmail.com>
2013-12-27 23:53:43 -0500

    Initial commit

---
+++ src/Rafasamp/Sonus/Sonus.php
@@ -0,0 +1,17 @@
+<?php namespace Rafasamp\Sonus;
+
+/**
+ * Laravel Audio Conversion Package
+ *
+ * This package is created to handle server-side audio conversion tasks using FFMPEG (http://www.fmpeg.org)
+ *
+ * @package    Laravel
+ * @category   Bundle
+ * @version    0.1
+ * @author     Rafael Sampaio <rafaelsampaio@live.com>
+ */
+
+class Sonus
+{
+	
+}
