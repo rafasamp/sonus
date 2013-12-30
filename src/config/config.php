77e45dbe649757c935434a7821cf9b0b56ede0d0
Rafael Sampaio <rafaicm@gmail.com>
2013-12-29 20:03:05 -0500

    Configuration file

--- src/config/config.php
+++ src/config/config.php
@@ -3,8 +3,16 @@
 return array(
 
     /*
-    |--------------------------------------------------------------------------
-    | Settings
-    |--------------------------------------------------------------------------
-    */
-)
+	|--------------------------------------------------------------------------
+	| FFMPEG System Path
+	|--------------------------------------------------------------------------
+	|
+	| We need to know the fully qualified system path to where ffmpeg
+	| lives on this server. This is the same path you would time in
+	| terminal to access FFMPEG.
+	|
+	*/
+   
+   'ffmpeg'    => '/Applications/XAMPP/xamppfiles/htdocs/laravel/public/ffmpeg',
+
+);
